567a1c6447e5c7cc3b59c2aaede39824b0b9307f
Joaquin <luduenajoa3@gmail.com>
2022-07-18 20:43:04 -0300

    Comenzando pagina principal

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="container-fluid">
-    
+    <nav class="navbar navbar-expand-md navbar-light bg-light border-bottom-">
+      <div class="container-fluid">
+      <router-link to="/home" id="name">💸Wallet</router-link>
+      <button type="button" class="navbar-toggler"
+      data-bs-toggle="collapse" data-bs-target="#MenuNavegacion">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div id="MenuNavegacion" class="collapse navbar-collapse">
+        <ul class="navbar-nav ms-3">
+          <li class="nav-item">Comprar</li>
+        </ul>
+      </div>
+      </div>
+    </nav>
   </div>
 </template>
 
